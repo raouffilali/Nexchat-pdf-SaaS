456fd3564711f4eba8038495e5efe8f5e776c427
Filali Abderaouf <filaliabderaouf54@gmail.com>
2023-10-04 14:22:00 +0100

    Setting TRPC

---
+++ src/components/Provider.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useState } from "react"
+import {QueryClient} from '@tanstack/react-query'
+import { trpc } from "@/app/_trpc/client";
+import { httpBatchLink } from "@trpc/react-query";
+
+const Providers = ()=>{
+    const [queryClient] = useState(() => new QueryClient());
+    const [trpcClient] = useState(()=> trpc.createClient({
+        links:[
+            httpBatchLink({
+                url: 'http//localhost:3000/api/trpc'
+            })
+        ]
+    }) )
+
+}
+
+export default Providers
